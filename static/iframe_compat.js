81dce22c7f21f625b196b9078709bd155b6ee450
Tim Schilling <schillingt@better-simple.com>
2023-10-09 07:57:05 -0500

    Don't remove the FAQs sub nav title from the iframe.
    The FAQ is much more confusing when we don't tell a person
    that every question's answer is YES.

--- static/iframe_compat.js
+++ static/iframe_compat.js
@@ -2,7 +2,7 @@ $(function() {
   console.log('rendered', window.parent, window, window.parent !== window)
   if (window.parent !== window) {
     // We're in an iframe. Let's remove the navbar and the footer
-    $('nav,footer').remove()
+    $('nav:not(.faq-nav),footer').remove()
     // With no navbar, the top padding for the content can be removed
     $('header.subpage-header').addClass('no-top')
   }
